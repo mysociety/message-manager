4ebfb309463e97c595f683c3101d2ea601b5a684
Dave Whiteland <dave@mysociety.org>
2012-06-15 15:41:48 +0100

    circumvent Apc cache for file cache (temporary) to run on deloyed server

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -28,19 +28,20 @@
  * Default settings provided below
  *
  * File storage engine.
- *
- * 	 Cache::config('default', array(
- *		'engine' => 'File', //[required]
- *		'duration'=> 3600, //[optional]
- *		'probability'=> 100, //[optional]
- * 		'path' => CACHE, //[optional] use system tmp directory - remember to use absolute path
- * 		'prefix' => 'cake_', //[optional]  prefix every cache file with this string
- * 		'lock' => false, //[optional]  use file locking
- * 		'serialize' => true, // [optional]
- * 		'mask' => 0666, // [optional] permission mask to use when creating cache files
- *	));
- *
- * APC (http://pecl.php.net/package/APC)
+ */
+
+	 Cache::config('default', array(
+		'engine' => 'File', //[required]
+		'duration'=> 3600, //[optional]
+		'probability'=> 100, //[optional]
+		'path' => CACHE, //[optional] use system tmp directory - remember to use absolute path
+		'prefix' => 'cake_', //[optional]  prefix every cache file with this string
+		'lock' => false, //[optional]  use file locking
+		'serialize' => true, // [optional]
+		'mask' => 0666, // [optional] permission mask to use when creating cache files
+	));
+ 
+ /* APC (http://pecl.php.net/package/APC)
  *
  * 	 Cache::config('default', array(
  *		'engine' => 'Apc', //[required]
--- app/Config/core.php
+++ app/Config/core.php
@@ -243,6 +243,8 @@ $engine = 'File';
 if (extension_loaded('apc') && function_exists('apc_dec') && (php_sapi_name() !== 'cli' || ini_get('apc.enable_cli'))) {
 	$engine = 'Apc';
 }
+$engine = 'File'; ###### FIXME:DW -- no, really!
+
 
 // In development mode, caches should expire quickly.
 $duration = '+999 days';
