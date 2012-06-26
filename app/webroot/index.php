ff5a2f735874c3610195043b22110bdcf1a58964
Struan Donald <struan@exo.org.uk>
2012-06-26 13:03:13 +0100

    if you are running under CGI/fastCGI PHP does not see the Basic Auth credentials so we have to set them manually

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -88,5 +88,16 @@
 
 	App::uses('Dispatcher', 'Routing');
 
+  // bit of a hack to make basic auth work with php running under (fast)cgi
+  if( !isset($_SERVER['PHP_AUTH_USER']) ) {
+    if (isset($_SERVER['HTTP_AUTHORIZATION']) && (strlen($_SERVER['HTTP_AUTHORIZATION']) > 0)) {
+        list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+        if( strlen($_SERVER['PHP_AUTH_USER']) == 0 || strlen($_SERVER['PHP_AUTH_PW']) == 0 ) {
+            unset($_SERVER['PHP_AUTH_USER']);
+            unset($_SERVER['PHP_AUTH_PW']);
+        }
+    }
+  }
+
 	$Dispatcher = new Dispatcher();
 	$Dispatcher->dispatch(new CakeRequest(), new CakeResponse(array('charset' => Configure::read('App.encoding'))));
