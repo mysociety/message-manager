6d2f9e457a5dcaa2e875f044abfb0b5461621878
Dave Whiteland <dave@mysociety.org>
2012-08-22 17:18:52 +0100

    hand-rolled migration, might need tweaking, as will schema

---
+++ db/schema_0001-add_sending_to_messages.sql
@@ -0,0 +1,25 @@
+
+# after chnages to messages for sending/receiving, modify messages, statuses and users tables
+
+# code uses 1 and 0 which is boolean in postgres
+ALTER TABLE messages
+  ADD COLUMN is_outbound boolean not null default 0;
+
+ALTER TABLE messages
+  ADD COLUMN parent_id integer;
+
+ALTER TABLE messages
+ RENAME COLUMN msisdn to from_address;
+
+ALTER TABLE messages
+  ADD COLUMN to_address varchar(128);
+
+INSERT INTO statuses 
+  (id, name, description) VALUES (4, 'pending', 'outbound message waiting to be sent');
+INSERT INTO statuses 
+  (id, name, description) VALUES (5, 'sent', 'outbound message has been sent');
+INSERT INTO statuses 
+  (id, name, description) VALUES (6, 'error', 'message send failed');
+
+ALTER TABLE users
+  ADD COLUMN can_reply boolean not null default 0;
