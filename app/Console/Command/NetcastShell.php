52206eb8a15cb1e71ec3b1406f6c3583328c7f17
Dave Whiteland <dave@mysociety.org>
2012-11-07 14:58:50 +0000

    added get_incoming (with dup checks) for pulling messages in from Netcast gateway

--- app/Console/Command/NetcastShell.php
+++ app/Console/Command/NetcastShell.php
@@ -4,9 +4,9 @@
  * Netcast shell commands:
  *
  * netcast gateway_test [source-name/id]
- * netcast getincoming [source-name/id]
+ * netcast get_incoming [source-name/id]
  *
- * Issue with: "cd app & Console/cake netcast gateway_test 1"
+ * Issue with: "cd app & Console/cake netcast gateway_test netcast-gateway"
  *
  *----------------------------------------------------------------*/
 class NetcastShell extends AppShell {
@@ -14,9 +14,18 @@ class NetcastShell extends AppShell {
 
 	public function getOptionParser() {
 	    $parser = parent::getOptionParser();
-		$parser->addArgument('source_id', array('help' => 'The id or name of the message souce (gateway)', 'required' => true));
-		$parser->addSubcommand('gateway_test', array('help' => 'Test the connection to the gateway (like pinging).'));
-		$parser->addSubcommand('get_incoming', array('help' => 'Pull down incoming messages from the gateway and load them into the database.'));
+		$parser->addArgument('source_id', array(
+					'help' => __('The id or name of the message souce (gateway)'), 'required' => true));
+		$parser->addSubcommand('gateway_test', array(
+					'help' => __('Test the connection to the gateway (like pinging).')));
+		$parser->addSubcommand('get_incoming', array(
+					'help' => __('Pull down incoming messages from the gateway and load them into the database.')));
+		$parser->addOption('allow-dups', array(
+					'help' => __('Save messages even if they already seem to be in the database (defaults to false, ' .
+								'so duplicate messages will be skipped).'), 
+					'boolean' => true,
+					'short' => 'a',
+					'default' => false));
 	    return $parser;
 	}
 	
@@ -38,17 +47,46 @@ class NetcastShell extends AppShell {
 		$this->check_url($ms);
 		$netcast = $this->get_netcast_connection($ms);
 		$ret_val = $this->call_netcast_function($netcast, $ms, "GETINCOMING");
+		$msgs_received = count($ret_val);
+		$msgs_skipped = 0;
+		$msgs_saved = 0;
+		$msgs_failed = 0;
 		if (is_array($ret_val)) {
-			$this->out(__("Received incoming messages: %s", count($ret_val)), 1, Shell::VERBOSE);
+			$this->out(__("Received incoming messages: %s", $msgs_received), 1, Shell::VERBOSE);
 			foreach ($ret_val as $msg) {
 				$this->out(__("Processing message: [%s] %s", $msg['min'], $msg['msg']), 1, Shell::VERBOSE);
-				// min => sender's number
-				// msg => message text
+				// checking for existing messages is a wee bit tricky cos the tag might need to be removed *sigh*
+				$conditions = array('Message.from_address' => $msg['min']);
+				$tag_data = Message::separate_out_tags($msg['msg']);
+				foreach ($tag_data as $key => $value) {
+					$conditions["Message.$key"] = $value;
+				}
+				$existing_msg = $this->Message->find('first', array('conditions' => $conditions, 'fields' => array('id')));
+				if (! ($this->params['allow-dups'] || empty($existing_msg))) {
+					$msgs_skipped++;
+					$this->out(__("   Skipping (message already exists in database with id=%s)", $existing_msg['Message']['id']), 1, Shell::VERBOSE);
+				} else {
+					$this->Message->create();
+					$this->Message->set('from_address', $msg['min']);
+					$this->Message->set('message', $msg['msg']);
+					$this->Message->set('source_id', $ms['id']);
+					$this->Message->set('is_outbound', 0);
+					$this->Message->set('status', Status::$STATUS_AVAILABLE);
+					if ($this->Message->save()) {
+						$msgs_saved++;
+						$this->out(__("   Saved OK"), 1, Shell::VERBOSE);
+					} else {
+						$msgs_failed++;
+						$this->out(__("   Saved FAILED"), 1, Shell::NORMAL);
+					}
+				}
 			}
+			$this->out(__("Incoming messages received: %s, saved: %s, skipped: %s, failed: %s", 
+				$msgs_received, $msgs_saved, $msgs_skipped, $msgs_failed), 1, Shell::NORMAL);
 			$this->out(__("Done"), 1, Shell::VERBOSE);
 		} else {
 			$ret_val = MessageSource::decode_netcast_retval($ret_val);
-			$this->out($ret_val, 1, Shell::QUIET);
+			$this->error("GETINCOMING fail", __("Gateway did not respond with a list: %s", $ret_val));
 		}
 	}
 	
