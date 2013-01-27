ced9175fcaf1122a927cc586718b1c5b111a3e8f
Dave Whiteland <dave@mysociety.org>
2013-01-27 00:17:34 +0000

    use read() not _load_record(), which doesn't exist outside of the Controller

--- app/Console/Command/NetcastShell.php
+++ app/Console/Command/NetcastShell.php
@@ -294,7 +294,7 @@ class NetcastShell extends AppShell {
 						if ($this->Message->save()) {
 							$msgs_saved++;
 							$parent_message = "";
-							self::_load_record($this->Message->id); // important and a wee bit sloppy: save and load the record to get the tags
+							$this->Message->read(null, $this->Message->id); // important and a wee bit sloppy: save and load the record to get the tags
 							$parent_message = $this->Message->autodetect_parent();
 							if (! empty($parent_message)) {
 								$this->Message->set('parent_id', $parent_message['id']);
@@ -302,7 +302,7 @@ class NetcastShell extends AppShell {
 									$parent_message = __(" (assumed to be a reply to message id=%s)", $parent_message['id']);
 								} // else... fail silently: the initial message was saved, but its reply-status was not; not a crisis
 							}
-							$this->out(__(" * Saved OK%s", $parent_message), 1, Shell::VERBOSE);
+							$this->out(__(" * Saved OK" . $parent_message), 1, Shell::VERBOSE);
 						} else {
 							$msgs_failed++;
 							$this->out(__(" * Saved FAILED"), 1, Shell::NORMAL);
