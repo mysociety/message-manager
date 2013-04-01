1ebdd595bdebfc0421b145d74ed12db36a38e662
Dave Whiteland <dave@mysociety.org>
2013-04-01 09:33:17 +0100

    set legacy messages to have message_received text

--- db/schema_0013-add_message_received_to_message.sql
+++ db/schema_0013-add_message_received_to_message.sql
@@ -1,1 +1,4 @@
 ALTER TABLE  `messages` ADD  `message_received` TEXT NULL DEFAULT NULL;
+
+-- not quite correct, but probably helpful: assume legacy messages have not been edited (although tags may have been stripped)
+UPDATE `messages` SET `message_received` = `message`;
